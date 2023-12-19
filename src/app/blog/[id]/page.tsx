1e6a671fc093e9aa09dc25dc51285eda79a0aaab
Otto Rahn <elias.yunusoff@gmail.com>
2023-12-19 22:49:26 +0500

    blog is done

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,59 @@
 import React from 'react'
+import styles from './page.module.scss'
+import Image from 'next/image'
 
 const BlogPost = () => {
-  return <div>BlogPost</div>
+  return (
+    <div>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.subtitle}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
+            dignissimos quas nesciunt, officia, magni eum reiciendis architecto
+            adipisci beatae aliquid nam itaque labore ea qui doloremque
+            obcaecati ad vero? Voluptatum.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Alex Smith</span>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
+          cupiditate molestias doloribus facere modi. Officiis, eos molestias
+          exercitationem impedit ullam maxime sapiente reiciendis sed esse
+          excepturi? Laborum cum iusto nostrum.
+          <br />
+          <br />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
+          cupiditate molestias doloribus facere modi. Officiis.
+          <br />
+          <br />
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+          voluptatum veniam error, officiis, facere sint maiores, dicta nobis
+          voluptas eveniet commodi culpa quasi eaque quia aperiam recusandae
+          quibusdam provident velit.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default BlogPost
--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import styles from './page.module.scss'
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
 
-const Category = () => {
-  return <div>Category</div>
+const Category = ({ params }: { params: { category: string } }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.subtitle}>Subtitle</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className={styles.img}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.subtitle}>Subtitle</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className={styles.img}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Category
